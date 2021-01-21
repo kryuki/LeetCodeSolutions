8a736a3e8eb0c23beb3968ce052a061350663564
Ryuki Kobayashi <kobayashiryuki1002@gmail.com>
2021-01-21 14:16:53 -0600

    add 012.py

---
+++ 012.py
@@ -0,0 +1,35 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        output = ""
+        L = len(str(num))
+        for i in range(L):
+            digit = int(str(num)[i])
+            cnt_5, mod_5 = digit // 5, digit % 5
+            add = ""
+            if L - i == 1:
+                if digit == 4:
+                    add = "IV"
+                elif digit == 9:
+                    add = "IX"
+                else:
+                    add = cnt_5 * "V" + mod_5 * "I"
+            elif L - i == 2:
+                if digit == 4:
+                    add = "XL"
+                elif digit == 9:
+                    add = "XC"
+                else:
+                    add = cnt_5 * "L" + mod_5 * "X"
+            elif L - i == 3:
+                if digit == 4:
+                    add = "CD"
+                elif digit == 9:
+                    add = "CM"
+                else:
+                    add = cnt_5 * "D" + mod_5 * "C"
+            else:
+                #L - i == 4
+                add = digit * "M"
+            output += add
+        
+        return output
