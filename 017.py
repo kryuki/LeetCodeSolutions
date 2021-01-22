1548203160965c6e4db58a6f97076fa64b834f9a
Ryuki Kobayashi <kobayashiryuki1002@gmail.com>
2021-01-21 20:22:28 -0600

    add backtrack

--- 017.py
+++ 017.py
@@ -24,4 +24,27 @@ class Solution:
                     output.append(newWord)
             
         return output
+    
+    #solution2: backtrack
+    #time: O(3^N * 4^M), space: O(3^N * 4^M)
+    #(N: # of digits that maps to 3 letters, M: # of digits that maps to 4 letters)
+    def letterCombinations2(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        output = []
+        numDic = {"2": ["a", "b", "c"], "3": ["d", "e", "f"],
+                  "4": ["g", "h", "i"], "5": ["j", "k", "l"], "6": ["m", "n", "o"],
+                  "7": ["p", "q", "r", "s"], "8": ["t", "u", "v"], "9": ["w", "x", "y", "z"]}
         
+        def backtrack(wordSoFar, idx):
+            nonlocal output
+            if idx == len(digits):
+                output.append(wordSoFar)
+            else:
+                digit = digits[idx]
+                button = numDic[digit]
+                for char in button:
+                    backtrack(wordSoFar + char, idx + 1)
+        
+        backtrack("", 0)
+        return output
